94a9e0e90022a0aebf08706423c2ab594e986960
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2025-07-16 19:45:26 -0400

    select new merge direction only after merge finished

--- pyawesim/example.py
+++ pyawesim/example.py
@@ -8,12 +8,13 @@ init_weather = WEATHER_SUNNY
 awesim_setup(sim, city_width, num_cars, 0.02, init_clock, init_weather)  # set's up sim variables, awesim map and adds cars randomly 
 sim_set_agent_enabled(sim, True)         # car 0 won't be NPC  
 decision_interval = 0.1
-total_play_time = 100.0
+total_play_time = 1000.0
 agent = sim_get_agent_car(sim)  
 sim_set_synchronized(sim, True, 1.0)  # synchronize simulation with real time  
 sim_connect_to_render_server(sim, "127.0.0.1", 4242)  # before this, start the server with `../bin/awesim_render_server 4242 --persistent`  
 
 current_procedure = "cruise"
+merge_direction = 'r'  # default merge direction
 
 print(f"Running simulation for {total_play_time} seconds with decision interval of {decision_interval} seconds. Press Ctrl+C to stop.")
 while sim_get_time(sim) < total_play_time:  
@@ -30,12 +31,12 @@ while sim_get_time(sim) < total_play_time:
             current_procedure = "merge"
     elif current_procedure == "merge":
         # every 5 seconds, change merging direction:
-        merge_direction = 'r' if sim_get_time(sim) % 10 < 5 else 'l'
         print(f"trying to merge {merge_direction}. current speed = {to_mph(speed)} mph.\n")
         merging_completed = merge(agent, sim, merge_direction, 5.0) # try to merge in the given direction with a 5 second duration
         if merging_completed:
             print("Merging completed")
             current_procedure = "cruise"
+            merge_direction = 'r' if sim_get_time(sim) % 10 < 5 else 'l'    # for next merge
 
     simulate(sim, decision_interval)    # simulate 0.1 seconds  
 print("Simulation finished.")
