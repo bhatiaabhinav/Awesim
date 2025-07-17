a26fe83658c0434db460bacafdc11190dc13d31c
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2025-07-17 03:47:06 -0400

    (broken) example of using procedures from python

--- pyawesim/example.py
+++ pyawesim/example.py
@@ -1,4 +1,5 @@
 from bindings import *
+import numpy as np
 
 sim = sim_malloc()
 city_width = 1000   # 1km
@@ -13,8 +14,9 @@ agent = sim_get_agent_car(sim)
 sim_set_synchronized(sim, True, 1.0)  # synchronize simulation with real time  
 sim_connect_to_render_server(sim, "127.0.0.1", 4242)  # before this, start the server with `../bin/awesim_render_server 4242 --persistent`  
 
-current_procedure = "cruise"
-merge_direction = 'r'  # default merge direction
+current_procedure = PROCEDURE_CRUISE
+procedure = sim_get_ongoing_procedure(sim, agent.id)  # get the ongoing procedure for the agent
+status = PROCEDURE_STATUS_NONE
 
 print(f"Running simulation for {total_play_time} seconds with decision interval of {decision_interval} seconds. Press Ctrl+C to stop.")
 while sim_get_time(sim) < total_play_time:  
@@ -23,20 +25,32 @@ while sim_get_time(sim) < total_play_time:
     distance_to_lead_vehicle = situation.distance_to_lead_vehicle   # example variable
     speed = car_get_speed(agent)
 
-    if current_procedure == "cruise":
-        print(f"trying to cruise. current speed = {to_mph(speed)} mph.\n")
-        cruising_completed = cruise(agent, from_mph(30))
-        if cruising_completed:
-            print(f"Agent {agent.id} is cruising at 30 mph.\n")
-            current_procedure = "merge"
-    elif current_procedure == "merge":
-        # every 5 seconds, change merging direction:
-        print(f"trying to merge {merge_direction}. current speed = {to_mph(speed)} mph.\n")
-        merging_completed = merge(agent, sim, merge_direction, 5.0) # try to merge in the given direction with a 5 second duration
-        if merging_completed:
-            print("Merging completed")
-            current_procedure = "cruise"
-            merge_direction = 'r' if sim_get_time(sim) % 10 < 5 else 'l'    # for next merge
+    if status != PROCEDURE_STATUS_IN_PROGRESS and status != PROCEDURE_STATUS_INITIALIZED:
+        if current_procedure == PROCEDURE_CRUISE:
+            print("Initializing CRUISE procedure...")
+            args = np.array([5.0, 13.0, 1.0, 2.0, 1.0], dtype=np.float64)  # cruise speed, follow distance, adaptive cruise control, preferred acceleration profile
+            c_ptr = args.__array_interface__['data'][0]
+            status = procedure_init(sim, agent, procedure, PROCEDURE_CRUISE, c_ptr)
+        elif current_procedure == PROCEDURE_MERGE:
+            print("Initializing MERGE procedure...")
+            merge_direction = INDICATOR_RIGHT if sim_get_time(sim) % 10 < 5 else INDICATOR_LEFT
+            args = np.array([float(merge_direction), 5.0, 13.0, 1.0, 2.0, 1.0], dtype=np.float64)
+            c_ptr = args.__array_interface__['data'][0]
+            status = procedure_init(sim, agent, procedure, PROCEDURE_MERGE, c_ptr)
+
+        if status != PROCEDURE_STATUS_INITIALIZED:
+            print(f"Agent {agent.id} failed to initialize procedure {current_procedure}. Status: {status}. Switching to default control for this step.\n")
+            status = PROCEDURE_STATUS_NONE
+    
+    if status != PROCEDURE_STATUS_NONE:
+        status = procedure_step(sim, agent, procedure)  # advance the procedure
+        if status != PROCEDURE_STATUS_IN_PROGRESS and status != PROCEDURE_STATUS_COMPLETED:
+            print(f"Agent {agent.id} failed to execute procedure {current_procedure}. Status: {status}\n")
+        if status != PROCEDURE_STATUS_IN_PROGRESS:
+            current_procedure = PROCEDURE_MERGE if current_procedure == PROCEDURE_CRUISE else PROCEDURE_CRUISE
+    else:
+        print(f"Agent {agent.id} is not executing any procedure. Current status: {status}\n")
+        car_reset_all_control_variables(agent)  # reset all control variables to default values
 
     simulate(sim, decision_interval)    # simulate 0.1 seconds  
 print("Simulation finished.")
