6ce9741fdd74102e31f7dd27c9f1d09a5dc81693
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2025-07-21 19:20:24 -0400

    improved compatibility across compilers and platforms

--- src/utils/time_utils.c
+++ src/utils/time_utils.c
@@ -1,14 +1,29 @@
-#include <sys/time.h>
 #ifdef _WIN32
 #include <windows.h>
+int gettimeofday(struct timeval *tp, void *tzp) {
+    static const unsigned __int64 epoch = 116444736000000000ULL;
+    FILETIME file_time;
+    SYSTEMTIME system_time;
+    unsigned __int64 time;
+
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime(&system_time, &file_time);
+    time = ((unsigned __int64)file_time.dwLowDateTime);
+    time += ((unsigned __int64)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec = (long)((time - epoch) / 10000000L);
+    tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+    return 0;
+}
 #else
+#include <sys/time.h>
 #include <time.h>
 #endif
 
 double get_sys_time_seconds() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (double)((long long)tv.tv_sec * 1000000LL + (long long)tv.tv_usec) / 1000000.0; // Convert milliseconds to seconds
+    return (double)((long long)tv.tv_sec * 1000000LL + (long long)tv.tv_usec) / 1000000.0; // Convert microseconds to seconds
 }
 
 void sleep_ms(int milliseconds) {
