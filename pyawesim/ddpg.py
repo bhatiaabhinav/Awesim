2f36ad3020376beed6bf5c3e245b6eb4ce545651
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2025-07-22 06:29:46 -0400

    ddpg model saving/loading

--- pyawesim/ddpg.py
+++ pyawesim/ddpg.py
@@ -25,6 +25,8 @@
 #     obs, rewards, dones, info = vec_env.step(action)
 #     env.render("human")
 
+import os
+import sys
 import gymnasium as gym
 
 import numpy as np
@@ -36,11 +38,20 @@ from bindings import *
 
 env = AwesimEnv(city_width=1000, num_cars=256, decision_interval=0.1, sim_duration=60 * 5, synchronized=False, synchronized_sim_speedup=200.0, should_render=False)
 
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+load_path = sys.argv[1] if len(sys.argv) > 1 else None
+if load_path is not None and not os.path.exists(load_path):
+    print(f"Model not found at {load_path}")
+    sys.exit(1)
 
-model = DDPG("MlpPolicy", env, policy_kwargs=dict(net_arch=[64, 64], n_critics=2), learning_starts=1000, batch_size=64, train_freq=1, action_noise=action_noise, verbose=1)
-model.learn(total_timesteps=20_000, log_interval=4)
+if load_path is not None:
+    print(f"Loading model from {load_path}")
+    model = DDPG.load(load_path, env=env)
+else:
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+    model = DDPG("MlpPolicy", env, policy_kwargs=dict(net_arch=[64, 64], n_critics=2), learning_starts=1000, batch_size=64, train_freq=1, action_noise=action_noise, verbose=1)
+    model.learn(total_timesteps=100_000, log_interval=4)
+    model.save("ddpg_awesim")
 
 env.should_render = True
 env.synchronized = True
